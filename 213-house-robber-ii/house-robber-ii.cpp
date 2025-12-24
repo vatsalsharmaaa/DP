524f17485aae2cd52f4dcffd167b786f5445365d
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2025-12-24 22:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (96.83%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[101];
+    int solve(vector<int>&nums,int i,int n){
+        if(i>n) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+
+        int steal= nums[i]+solve(nums,i+2,n);
+        int pass=solve(nums,i+1,n);
+        return dp[i]=max(pass,steal);
+    }
+    int rob(vector<int>& nums) {
+        int n= nums.size();
+        if(n==1) return nums[0];
+
+        if(n==2)
+        return max(nums[0],nums[1]);
+
+        memset(dp,-1,sizeof(dp));
+
+        //case1 pehle ghar ko lele
+        int taking_1st=solve(nums,0,n-2);
+ memset(dp,-1,sizeof(dp));
+        //case2 pehla ghar nahi lia
+        int taking_2nd = solve(nums,1,n-1);
+
+        return max(taking_1st,taking_2nd);
+    }
+};
