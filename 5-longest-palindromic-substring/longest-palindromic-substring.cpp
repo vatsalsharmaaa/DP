6547b2dd27f1ffbcbc647df9209b4e3058ff6e38
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-08 23:12:44 +0530

    Time: 322 ms (15.38%) | Memory: 26.2 MB (25.98%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,73 @@
-class Solution {
-public:
+// REC + MEMO
+// class Solution {
+// public:
     int dp[1001][1001];
 
-    bool solve(string &s,int i,int j){
-        if(i>=j){
-            return true;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==s[j]){
-            return dp[i][j]= solve(s,i+1,j-1);
-        }
+//     bool solve(string &s,int i,int j){
+//         if(i>=j){
+//             return true;
+//         }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         if(s[i]==s[j]){
+//             return dp[i][j]= solve(s,i+1,j-1);
+//         }
 
-        return dp[i][j] = false;
-    }
+//         return dp[i][j] = false;
+//     }
+//     string longestPalindrome(string s) {
+//         int n= s.length();
+//         memset(dp,-1,sizeof(dp));
+//         int maxlen=INT_MIN;
+
+//         int sp=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(solve(s,i,j)==true){
+//                     if(j-i+1>maxlen){
+//                     maxlen=j-i+1;
+//                     sp=i;
+//                   }
+//                 }
+//             }
+//         }
+//         return s.substr(sp,maxlen);
+//     }
+// };
+
+class Solution {
+public:
     string longestPalindrome(string s) {
-        int n= s.length();
-        memset(dp,-1,sizeof(dp));
-        int maxlen=INT_MIN;
+        int n = s.length();
+        if (n == 0) return "";
 
-        int sp=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j)==true){
-                    if(j-i+1>maxlen){
-                    maxlen=j-i+1;
-                    sp=i;
-                  }
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        int maxlen = 1;   // at least 1 char palindrome
+        int sp = 0;
+
+        // L = length of substring
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L - 1 < n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    dp[i][j] = true;               // length 1
+                }
+                else if (i + 1 == j) {
+                    dp[i][j] = (s[i] == s[j]);     // length 2
+                }
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+                }
+
+                if (dp[i][j] && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    sp = i;
                 }
             }
         }
-        return s.substr(sp,maxlen);
+        return s.substr(sp, maxlen);
     }
 };
