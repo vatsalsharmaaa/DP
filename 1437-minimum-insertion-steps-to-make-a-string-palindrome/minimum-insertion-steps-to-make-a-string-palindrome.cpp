7f8b2aff33932787bee1d77a482cbe1583500cfb
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2026-02-09 20:36:30 +0530

    Time: 11 ms (98.08%) | Memory: 9.8 MB (86.27%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,50 @@
+// class Solution {
+// public:
+//     int minInsertions(string str) {
+//         string rev = str;
+//         reverse(rev.begin(), rev.end());
+//         int n = str.size();
+
+//         // LCS table
+//         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+//         for (int i = 1; i <= n; ++i) {
+//             for (int j = 1; j <= n; ++j) {
+//                 if (str[i - 1] == rev[j - 1])
+//                     dp[i][j] = 1 + dp[i - 1][j - 1];
+//                 else
+//                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+
+//         return n - dp[n][n]; // Minimum insertions = total length - LPS length
+//     }
+// };
+
+class Solution {
+public:
+    int dp[501][501];
+    int n;
+
+    int solve(int i, int j, string &s) {
+        if (i >= j) return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] == s[j]) {
+            return dp[i][j] = solve(i + 1, j - 1, s);  // ✅ FIXED
+        } else {
+            int take1 = 1 + solve(i + 1, j, s);
+            int take2 = 1 + solve(i, j - 1, s);
+
+            return dp[i][j] = min(take1, take2);
+        }
+    }
+
+    int minInsertions(string str) {
+        n = str.size();
+        memset(dp, -1, sizeof(dp));
+        return solve(0, n - 1, str);
+    }
+};
